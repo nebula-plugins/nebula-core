76e815174e4659f71eda5b80614f03b70f75142c
Justin Ryan <jryan@netflix.com>
2014-02-16 23:03:53 -0800

    Fix test

--- src/test/groovy/nebula/core/tasks/UntarProjectSpec.groovy
+++ src/test/groovy/nebula/core/tasks/UntarProjectSpec.groovy
@@ -6,7 +6,7 @@ class UntarProjectSpec extends ProjectSpec {
     def 'create task'() {
         when:
         def downloadTask = project.task([type:Download], 'download')
-        def uploadTask = (Unzip) project.task([type:Untar], 'untar')
+        def uploadTask = (Untar) project.task([type:Untar], 'untar')
         uploadTask.from(downloadTask)
 
         then:
